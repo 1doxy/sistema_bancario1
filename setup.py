a78fdf5f9e66a64b81894d602fde3e825cf5424e
1doxy <hackedbydoxy@gmail.com>
2024-10-11 12:23:51 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sistema_bancario',
+    version='0.1',
+    packages=find_packages(),
+    description='Um sistema bancário simples em Python.',
+    author='Matheus Taranto',
+    author_email='matheus.taranto138@gmail.com',
+    url='https://github.com/1doxy/sistema_bancario1',  # Link para o repositório
+    python_requires='>=3.6',
+)
